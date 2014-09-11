efa9c56764eaaa29a52a9ab86ff3ab1988a8bfb1
Nikki Durkin <nikki@99dresses.com>
2014-09-11 17:30:17 +1000

    added test class

---
+++ NDtest.h
@@ -0,0 +1,13 @@
+//
+//  NDtest.h
+//  Git Test
+//
+//  Created by Nikki Durkin on 9/11/14.
+//  Copyright (c) 2014 Nikki Durkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NDtest : NSObject
+
+@end
---
+++ NDtest.m
@@ -0,0 +1,13 @@
+//
+//  NDtest.m
+//  Git Test
+//
+//  Created by Nikki Durkin on 9/11/14.
+//  Copyright (c) 2014 Nikki Durkin. All rights reserved.
+//
+
+#import "NDtest.h"
+
+@implementation NDtest
+
+@end
